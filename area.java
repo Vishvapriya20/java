7f27543b942fe821cf5e5f6d9d8141c190ec182f
Vishvapriya20 <82632054+Vishvapriya20@users.noreply.github.com>
2021-08-16 15:49:40 +0530

    Update area.java

--- area.java
+++ area.java
@@ -1,11 +1,11 @@
 public class Main
 {
 	public static void main(String[] args) {
-	    int diff=23,peri=206,area;
-	    int sumoflb=peri/2;
+	    int difference=23,perimeter=206,area;
+	    int sumoflb=perimeter/2;
 	    int dlength=sumoflb+diff;
-	    int len=dlength/2;
+	    int length=dlength/2;
 	    int breath=sumoflb-len;
-		System.out.println(len*breath);
+	    System.out.println(length*breath);
 	}
 }
